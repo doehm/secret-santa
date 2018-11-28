f1fad88ff6b14144a7859cd113316ac233980c0e
doehm <danieloehm@gmail.com>
2018-11-28 21:29:50 +1100

    Added notes

--- secretSantaTemplate.py
+++ secretSantaTemplate.py
@@ -1,18 +1,19 @@
-#SECRET SANTA
+# SECRET SANTA
 
-#LIBRARIES
+# LIBRARIES
 import os
 import numpy as np,
 import datetime
 import pandas as pd
 import smtplib
 
-#DATE AND TIME
+# DATE AND TIME
 dt = datetime.datetime.now()
 date = dt.strftime('%Y-%m-%d at %I:%M%p')
 datem = dt.strftime('%Y-%m-%d')
 
-#EMAIL DICTIONARY
+# EMAIL DICTIONARY
+# replace the names and emails with those participating
 fam = {
 'Alice'   : 'alice@gmail.com',
 'Bob'     : 'bob@gmail.com',
@@ -28,15 +29,15 @@ recvr = []
 for k in range(-1, len(santa)-1):
 	recvr.append(santa[k])
 			
-#TESTING FOR ACCURATE SELECTIONS
+# TESTING FOR ACCURATE SELECTIONS
 for k in range(len(fam)):
 	print santa[k] != recvr[k]
 			
-#STORE SELECTIONS FOR SAFE KEEPING
+# STORE SELECTIONS FOR SAFE KEEPING
 santadf = pd.DataFrame(fam.items(), columns = ['santa', 'recvr'])
 santadf.to_csv('Secret Santa list ' + datem + '.csv')
 	
-#STARTING EMAILS
+# STARTING EMAILS
 smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
 smtpObj.ehlo()
 smtpObj.starttls()
@@ -44,17 +45,16 @@ eml = raw_input('Enter your email: ')
 pwd = raw_input('Enter login details: ')
 smtpObj.login(eml, pwd)
 
-#SENDING EMAILS
+# SENDING EMAILS
 for k in range(len(fam)):
 	smtpObj.sendmail(eml, fam[santa[k]], \
 	'Subject: Secret Santa 2018 \
 	\nHo Ho Ho %s! \
 	\n\nChristmas is almost here, \
 	\nTime to find that Christmas cheer! \
-	\n\nhttps://media1.giphy.com/media/yXUu66ZPiiKIM/giphy.gif?cid=3640f6095be0256954394f3432f75df3\
 	\n\nThis year you are buying for....... %s! \
 	\n\nThis was sent on %s. \
 	\n\nDan' % (santa[k], recvr[k], date))
 
-#QUIT SERVER
+# QUIT SERVER
 smtpObj.quit()
